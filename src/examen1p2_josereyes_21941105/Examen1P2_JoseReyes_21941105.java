5f5d66c8bae79138ec1de8713c290d7a29dd7f36
jose13001 <josecarlos_01_30@unitec.edu>
2022-02-18 17:50:42 -0600

    opciones universo terminada creo

--- src/examen1p2_josereyes_21941105/Examen1P2_JoseReyes_21941105.java
+++ src/examen1p2_josereyes_21941105/Examen1P2_JoseReyes_21941105.java
@@ -51,6 +51,45 @@ public class Examen1P2_JoseReyes_21941105 {
     
     public static void menuUniverso(){
         int opc1;
+        do{
+            System.out.println("OPCIONES UNIVERSO");
+            System.out.println("0...Salir");
+            System.out.println("1...Crear Universo");
+            System.out.println("2...Modificar Universo");
+            System.out.println("3...Eliminar Universo");
+            System.out.println("4...Listar Universos");
+            System.out.println("Ingrese una opcion");
+            opc1=lea.nextInt();
+            switch(opc1){
+                case 1:
+                    System.out.println("***Creando Universo***");
+                    System.out.println("Como llamaras a este nuevo universo: ");
+                    String Universo=lea.next();
+                    universo.add(new Universo(Universo));
+                    break;
+                case 2:
+                    System.out.println("Ingrese la posicion del universo que desea modificar: ");
+                    int pos=lea.nextInt();
+                    System.out.println("Ingrese un nuevo nombre:  ");
+                    Universo =lea.next();
+                    universo.get(pos).setNombre(Universo);
+                    break;
+                case 3:  
+                    System.out.println("Que universo deseas eliminar el dia de hoy?");                   
+                    int delete=lea.nextInt();
+                    universo.remove(delete);
+                    
+                    break;
+                case 4:
+                    System.out.println("Lista de Universos");
+                    int i=0;
+                    for(Universo U: universo){
+                        i++;
+                        System.out.println(i+" "+U);
+                    }
+                    break;
+            }
+        }while(opc1!=0);
         
     }
     
